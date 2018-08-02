5f2c3fb14c0b371b515245571eb78d10e5d0dc7b
jean <nguyen.jean@me.com>
2018-08-02 10:13:55 +0200

    push 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_202445) do
+ActiveRecord::Schema.define(version: 2018_08_02_072918) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
